2cae40c23664f040f0ada7067234b667adb869b3
Fer_Stock <monterofernandoe@gmail.com>
2023-09-21 14:35:20 -0300

    *

--- app/models/servers_model.py
+++ app/models/servers_model.py
@@ -15,5 +15,34 @@ class Server:
         return self.__dict__
     
     @classmethod
-    def create(cls,data):
-        pass
+    def create(cls,server):
+        query="INSERT INTO teamhub.servers (name,description,img,user_id,category_id) VALUES(%s,%s,%s,%s,%s)"
+        params=(server.name,server.description,server.img,server.user_id,server.category_id)
+        DatabaseConnection.execute_query(query,params)
+    
+    @classmethod
+    def get_all(cls):
+        query="SELECT * FROM teamhub.servers"
+        servers=DatabaseConnection.fetchall(query)
+        return [cls(**dict(zip(cls._keys,row))) for row in servers]
+    @classmethod
+    def get(cls,data):
+        keys=''.join("{}=%s".format(key) for key in data.keys())
+        query=f"SELECT * FROM teamhub.servers WHERE {keys}=%s"
+        params=tuple(data,)
+        response=DatabaseConnection.fetchone(query,params)
+        return cls(**dict(zip(cls._keys,response)))
+    @classmethod
+    def update(cls,data):
+        keys=' ,'.join("{}=%s".format (key) for key in data.keys() if key != 'id')
+        query=f"UPDATE teamhub.servers SET {keys} WHERE id=%s"
+        params=tuple(param for k,param in data.items() if k != 'id')+(data['id'],)
+        DatabaseConnection.execute_query(query,params)
+    @classmethod
+    def delete(cls,data):
+        query="DELETE FROM teamhub.servers WHERE id=%s"
+        params=(data['id'],)
+        DatabaseConnection.execute_query(query,params)
+        
+
+        
